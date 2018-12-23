3297e05c7b24af00a0b93e25f3934e9996d0b4a9
pzb568 <pengzb886@gmail.com>
2018-12-23 14:15:31 +0800

    上传python文件

--- 02交互问侯.py
+++ 02交互问侯.py
@@ -3,3 +3,5 @@ print(f"你好，{name}！")
 """
 输入名字显示 某某，你好
 """
+name=input("请输入姓名")
+print("你好,%s! % name)
---
+++ 9.py
@@ -0,0 +1,2 @@
+
+in([" ".join(["{}*{}={}".format(y,x,x*y)foryinrange(1,x+1)])forxinrange(1,10)]))
